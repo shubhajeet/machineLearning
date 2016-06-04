aca8d1fa2993f56e360f5c6f54660bd87a1fe673
sujit maharjan <mail@sujit.email.np>
2016-06-04 09:48:29 +0545

    neural network backward propogation implemented

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -61,19 +61,47 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-iLayer = [ones(length(X),1) X];
-hLayer = sigmoid(iLayer * Theta1');
 
-hLayer = [ones(length(hLayer),1) hLayer];
-h = sigmoid(hLayer * Theta2');
+% Part 1: Feedforward neural network calcuation of cost function
 
+a_1 = [ones(size(X,1),1) X];         % input Layer + bias parameter
+z_2 = a_1 * Theta1';     % calculation of hidden layer
+a_2 = sigmoid(z_2);
+a_2 = [ones(size(a_2,1),1) a_2];
+z_3 = a_2 * Theta2'; % hidden Layer + bias parameter
+a_3 = sigmoid(z_3);            % calcuation of output layer
+h = a_3;
+
+%a_1 = X;         % input Layer + bias parameter
+%z_2 = [ones(size(a_1,1),1) a_1] * Theta1';     % calculation of hidden layer
+%a_2 = sigmoid(z_2);
+%z_3 = [ones(size(a_2,1),1) a_2] * Theta2'; % hidden Layer + bias parameter
+%a_3 = sigmoid(z_3);            % calcuation of output layer
+%h = a_3;
+
+% Part 2.1 output vector preparation 
 Y = [y==1];
 for i = 2:1:num_labels
   Y = [Y y==i];
 endfor
+
+% Calcuation of cost function
 J = -Y .* log(h) - (ones(size(Y))-Y) .* log(ones(size(h))-h);
 J = sum(sum(J));
 J = J/m;
+
+% Part 2: Back propogation algorithm
+
+del3 = a_3 - Y; 
+del2 = (del3 * Theta2) .* sigmoidGradient([ones(size(z_2,1),1) z_2]);
+Delta_1 = del2(:, 2:end)' * a_1;
+%Delta_1 = Delta_1(2:end);
+Delta_2 = del3' * a_2;
+%Delta_2 = Delta_2(2:end);
+Theta1_grad = Delta_1./m;
+Theta2_grad = Delta_2./m;
+% Part 3: Implemenation of regularized cost function and gradient
+
 Theta1(:,1) = zeros(size(Theta1,1),1);
 Theta2(:,1) = zeros(size(Theta2,1),1);
 J = J + lambda/2/m * (sum(sum(Theta1.*Theta1)) + sum(sum(Theta2.*Theta2)));
